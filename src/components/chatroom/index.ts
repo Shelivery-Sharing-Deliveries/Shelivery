a9e0cd117f79e01fa3e6bfcd3d867e5c38ea0da1
AradFr <aradfr@gmail.com>
2025-07-19 15:30:57 +0200

    1111

--- src/components/chatroom/index.ts
+++ src/components/chatroom/index.ts
@@ -2,7 +2,6 @@ export { ChatHeader } from './ChatHeader';
 export { ChatMessages } from './ChatMessages';
 export { ChatInput } from './ChatInput';
 export { MessageInput } from './MessageInput';
-export { AdminPanel } from './AdminPanel';
 export { MembersList } from './MembersList';
 export { ChatNotifications } from './ChatNotifications';
 export { OrderStatusCard } from './OrderStatusCard';
