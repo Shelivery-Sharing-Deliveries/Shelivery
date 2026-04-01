c4d587f6a9a645de8acd82efcb175cafb2fdd811
ims9898 <96818795+ims9898@users.noreply.github.com>
2026-04-01 10:28:33 +0000

    Vercel caching off

--- src/app/api/shops/[shopId]/blog/route.ts
+++ src/app/api/shops/[shopId]/blog/route.ts
@@ -5,6 +5,9 @@ import type { Database } from '@/lib/database.types'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { shopId: string } }
